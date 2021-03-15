845cbc46c0d7f7060425a03faa9d481b49858571
Ekaterin22 <77670720+Ekaterin22@users.noreply.github.com>
2021-03-15 11:53:09 +0400

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+  let staple = bracketsConfig.join('').replace(/,/g, '');
+      let mass = [];
+  for (let bracket of str) {
+          let stapleIndex = staple.indexOf(bracket)
+
+          if (stapleIndex % 2 === 0) {
+              
+              if (bracket === staple[stapleIndex + 1] && mass[mass.length - 1] === stapleIndex){
+                mass.pop();
+              } else if (bracket === staple[stapleIndex + 1] && mass[mass.length - 1] !== stapleIndex) {
+                mass.push(stapleIndex)
+              }
+              else{
+                mass.push(stapleIndex)
+              }
+          } 
+          else {
+              if (mass.pop() !== stapleIndex-1){
+                  return false;
+              }
+          }
+          
+      }
+      return mass.length === 0
+  
 }
